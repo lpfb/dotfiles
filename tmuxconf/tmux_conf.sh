59c8e354be25e110b6b4968fcfa57fa07d797a13
Luis Paulo Fernandes de Barros <lbarros@cpqd.com.br>
2019-08-02 13:57:20 -0300

    Adiciona nova versao de tmux melhorada Configuracoes usadas: > https://github.com/samoshkin/tmux-config

--- tmuxconf/tmux_conf.sh
+++ tmuxconf/tmux_conf.sh
@@ -1,4 +1,2 @@
-cd
-git clone https://github.com/gpakosz/.tmux.git
-ln -s -f .tmux/.tmux.conf
-cp .tmux/.tmux.conf.local .
+git clone https://github.com/samoshkin/tmux-config.git
+./tmux-config/install.sh
