62fc5f3c8d8bf1c68083662acf0146222996611b
Luis Paulo Fernandes de Barros <lbarros@cpqd.com.br>
2023-09-14 15:59:52 -0300

    Add revised procedures

--- tmuxconf/readme.txt
+++ tmuxconf/readme.txt
@@ -1,6 +1,15 @@
-After install tmux, it is necessaty to install xsel to copy work properly:
+# Instalar a powerline
+1) git clone https://github.com/powerline/fonts.git
+2) cd fonts
+3) ./install.sh
+4) editar .tmux.conf e descomentar as linhas:
+    tmux_conf_theme_left_separator_main='\uE0B0'
+    tmux_conf_theme_left_separator_sub='\uE0B1'
+    tmux_conf_theme_right_separator_main='\uE0B2'
+    tmux_conf_theme_right_separator_sub='\uE0B3'
+5) Sair de todos os terminas e sessoes de tmux e reabrir 
 
-sudo apt-get install tmux
-sudo apt-get install xsel
-
-Now move tmux_conf to $HOME/.tmux_conf
+# Habilitar powerline no temrinal
+1) Abrir o terminal e clicar com o botao direito
+2) preferences/Test/Custom fonts: Source Code Pro for Powerline
+3) Sair de todos os terminas e reabrir 
--- vim/vim_script.sh
+++ vim/vim_script.sh
@@ -1,35 +1,12 @@
 #!/bin/bash
-cd ~
 
 sudo apt-get install python3-dev
 sudo apt-get update && sudo apt-get install build-essential
 
-git clone https://github.com/vim/vim.git
-cd vim
-
-./configure --with-features=huge \
-            --enable-multibyte \
-            --enable-rubyinterp=yes \
-            --enable-python3interp=yes \
-            --with-python3-config-dir=$(python3-config --configdir) \
-            --enable-perlinterp=yes \
-            --enable-luainterp=yes \
-            --enable-gui=gtk2 \
-            --enable-cscope \
-            --prefix=/usr/local
-
-make VIMRUNTIMEDIR=/usr/local/share/vim/vim90
-
-echo "export VIMRUNTIME=/usr/share/vim/vim90" >> ~/.bashrc
-
-ln -s ~/my_git_repos/vim/vimrc ~/.vimrc
-
-sudo apt-get install python3-dev
-sudo apt-get update && sudo apt-get install build-essential
+ln -s vimrc ~/.vimrc
 
 cd
 mkdir ~/.vim/bundle -p
-
 cd ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git
 vim
