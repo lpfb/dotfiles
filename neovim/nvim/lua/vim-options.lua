be15a444ed34048f4ce363a11a0af98c393753de
Luis Paulo Fernandes de Barros <lbarros@cpqd.com.br>
2024-03-22 13:35:49 -0300

    Add ESC and split shortcuts

--- neovim/nvim/lua/vim-options.lua
+++ neovim/nvim/lua/vim-options.lua
@@ -14,16 +14,24 @@ vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
+-- Remap ESC to Ctrl+j
+vim.keymap.set("i","<c-j>","<Esc>")
+
 -- Retab all file
 vim.keymap.set('n', '<leader>t', ':retab<CR>')
 
 -- Remove search hoghlight
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set('n', '<leader>n', ':nohlsearch<CR>')
 
 -- Salve file using Ctrl+s
 vim.keymap.set('n', '<c-s>', ':w!<CR>')
+
 -- Quit vim using Ctrl+q
 vim.keymap.set('n', '<c-q>', ':q<CR>')
+
+-- Splitting settings
+vim.keymap.set('n', '<leader>h', ':vs<CR>')
+vim.keymap.set('n', '<leader>v', ':sp<CR>')
 
 vim.wo.number = true
 
