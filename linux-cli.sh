e4f3f70e1c2484cb4b139c9efdcf6d607a117852
Luis Paulo Fernandes de Barros <lbarros@cpqd.com.br>
2024-12-04 12:29:11 -0300

    Add new linux cli tools install and .gitconfig better diff

---
+++ linux-cli.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt install duf # Disk Usage. Better than df
+sudo apt install exa # List files. Better than ls/ll
+sudo snap install diff-so-fancy # Diff better than linux one. Just works on snap.
