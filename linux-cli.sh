b3954f8ffc219687aad3d76ee6e694daa0492f32
Luis Paulo Fernandes de Barros <lbarros@cpqd.com.br>
2024-12-23 08:37:09 -0300

    Add htop alternative

--- linux-cli.sh
+++ linux-cli.sh
@@ -3,3 +3,4 @@
 sudo apt install duf # Disk Usage. Better than df
 sudo apt install exa # List files. Better than ls/ll
 sudo snap install diff-so-fancy # Diff better than linux one. Just works on snap.
+sudo apt install bpytop
